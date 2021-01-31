1ed6ff9da17d22f32148dfed22040aeeefcdb107
HyunHub <19924___@naver.com>
2021-01-31 16:04:44 +0900

    initialization commit

---
+++ hello-react/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() { // APP 이라는 Component를 만들어 줌 - 함수형 Component, 밑 코드는 HTML이 아니고 JSX(javaScript의 확장 문법)라고 부름
+  const name = 'React'; // const - 한 번 설정하면 재설정 불가, let 선언 후 유동적 변할 수 있을 때 사용 for 문,, 둘 다 같은 블록 내 중복 선언 불가능
+/*   return ( // <>는 Fragment 생략된 거,, 여러 요소가 무조건 부모 요소 하나에 감싸져 있어야 해서 씀. div를 쓰기 싫을 때 
+ <> 
+    {name === 'React' ? (
+    <h1>Hello {name} !!</h1>
+    ) : (
+    <h2>안녕 리액트</h2> 
+    )}
+    </>
+  ); */
+
+  //return <div>{name ==='React' && <h1>React !!</h1>}</div>;
+
+  const ud = undefined;
+
+ // return ud || 'undefined 입니다'; undefined 입니다 출력
+ // return <div>{ud || 'React'}</div>;  React 출력
+ // return <div>{ud}</div>;  JSX 내부에서 렌더링 하는 것은 괜찮, 하지만 return ud; 는 불가능 
+ 
+}
+
+export default App;
