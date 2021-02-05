7b605a7877e80967b92692115768760857d84934
HyunHub <19924___@naver.com>
2021-02-05 23:44:52 +0900

    Event Handler

--- hello-react2/src/EventPractice.js
+++ hello-react2/src/EventPractice.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+/* import React, { Component } from "react";
 
 class EventPractice extends Component {
   state = {
@@ -32,6 +32,54 @@ class EventPractice extends Component {
         >
           확인
         </button>
+      </div>
+    );
+  }
+}
+
+export default EventPractice;
+ */
+
+import React, { Component } from "react";
+
+class EventPractice extends Component {
+  state = {
+    message: "",
+  };
+
+  constructor(props) {
+    super(props);
+    // 임의 메서드가 이벤트로 등록되어도 this를 컴포넌트 자신으로 제대로 가리키기 위해서는
+    // method를 this와 binding - 하지 않으면 this가 undefined를 가리키게 됨.
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      message: e.target.value,
+    });
+  }
+
+  handleClick(e) {
+    alert(this.state.message);
+    this.setState({
+      message: "",
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Event Practice</h1>
+        <input
+          type="text"
+          name="message"
+          placeholder="Write Here"
+          value={this.state.message}
+          onChange={this.handleChange}
+        />
+        <button onClick={this.handleClick}>확인</button>
       </div>
     );
   }
