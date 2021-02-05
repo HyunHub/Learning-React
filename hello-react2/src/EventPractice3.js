e763265681fe241a5d94c606c3497aee7b7ec05e
HyunHub <19924___@naver.com>
2021-02-06 00:30:12 +0900

    useState - Form

--- hello-react2/src/EventPractice3.js
+++ hello-react2/src/EventPractice3.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+/* import React, { useState } from "react";
 
 const EventPractice3 = () => {
   const [username, setUsername] = useState("");
@@ -33,6 +33,61 @@ const EventPractice3 = () => {
         placeholder="아무거나 입력"
         value={message}
         onChange={onChangeMessage}
+        onKeyPress={onKeyPress}
+      />
+      <button onClick={onClick}>확인</button>
+    </div>
+  );
+};
+
+export default EventPractice3;
+ */
+
+import React, { useState } from "react";
+
+const EventPractice3 = () => {
+  const [form, setForm] = useState({
+    username: "",
+    message: "",
+  });
+
+  const { username, message } = form;
+  const onChange = (e) => {
+    const nextForm = {
+      ...form,
+      [e.target.name]: e.target.value,
+    };
+    setForm(nextForm);
+  };
+  const onClick = () => {
+    alert(username + ": " + message);
+    setForm({
+      username: "",
+      message: "",
+    });
+  };
+
+  const onKeyPress = (e) => {
+    if (e.key == "Enter") {
+      onClick();
+    }
+  };
+  return (
+    <div>
+      <h1>Event Practice</h1>
+      <input
+        type="text"
+        name="username"
+        placeholder="사용자명"
+        value={username}
+        onChange={onChange}
+      />
+      <input
+        type="text"
+        name="message"
+        placeholder="아무거나 입력"
+        value={message}
+        onChange={onChange}
         onKeyPress={onKeyPress}
       />
       <button onClick={onClick}>확인</button>
