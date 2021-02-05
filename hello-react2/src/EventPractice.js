0b95aa8fdf2000e4198d1c499e1078048c7d705f
HyunHub <19924___@naver.com>
2021-02-06 00:05:30 +0900

    Property Initializer Syntax

--- hello-react2/src/EventPractice.js
+++ hello-react2/src/EventPractice.js
@@ -47,7 +47,7 @@ class EventPractice extends Component {
     message: "",
   };
 
-  constructor(props) {
+  /* constructor(props) {
     super(props);
     // 임의 메서드가 이벤트로 등록되어도 this를 컴포넌트 자신으로 제대로 가리키기 위해서는
     // method를 this와 binding - 하지 않으면 this가 undefined를 가리키게 됨.
@@ -66,7 +66,20 @@ class EventPractice extends Component {
     this.setState({
       message: "",
     });
-  }
+  } */
+
+  handleChange = (e) => {
+    this.setState({
+      message: e.target.value,
+    });
+  };
+
+  handleClick = (e) => {
+    alert(this.state.message);
+    this.setState({
+      message: "",
+    });
+  };
 
   render() {
     return (
