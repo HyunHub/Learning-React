c141a69c1a2c588a350497bf3cfd6194b857f80c
HyunHub <19924___@naver.com>
2021-02-06 00:30:33 +0900

    -

--- hello-react2/src/App.js
+++ hello-react2/src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import EventPractice from "./EventPractice";
+// import EventPractice2 from "./EventPractice2";
+import EventPractice3 from "./EventPractice3";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  /* return <EventPractice />; */
+  /* return <EventPractice2 />; */
+  return <EventPractice3 />;
+};
 
 export default App;
