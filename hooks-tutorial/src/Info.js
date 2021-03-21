aa5d1d77a329b6eff5263a76dc9e746c84ba71cd
HyunHub <19924___@naver.com>
2021-03-21 14:16:54 +0900

    useEffect - cleanup

--- hooks-tutorial/src/Info.js
+++ hooks-tutorial/src/Info.js
@@ -5,18 +5,32 @@ const Info = () => {
   const [nickname, setNickname] = useState("");
 
   // 렌더링 될 때마다 콘솔에 찍힘
-  useEffect(() => {
-    console.log("렌더링이 완료되었습니다");
-    console.log({
-      name,
-      nickname,
-    });
-  });
+  // useEffect(() => {
+  //   console.log("렌더링이 완료되었습니다");
+  //   console.log({
+  //     name,
+  //     nickname,
+  //   });
+  // });
 
-  // // 컴포넌트가 처음 나타날 때만 콘솔에 문구가 나옴
+  // 컴포넌트가 처음 나타날 때만 콘솔에 문구가 나옴
   // useEffect(() => {
   //   console.log("마운트 될 때만 실행됩니다");
   // }, []);
+
+  // 특정 값이 변경될 때만 호출. 두 번쨰 파라미터로 전달되는 배열 안에 검사하고 싶은 값 넣으면 됨
+  // useEffect(() => {
+  //   console.log(name);
+  // }, [name]);
+
+  useEffect(() => {
+    console.log("effect");
+    console.log(name);
+    return () => {
+      console.log("cleanup");
+      console.log(name);
+    };
+  }, [name]);
 
   const onChangeName = (e) => {
     setName(e.target.value);
