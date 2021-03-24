7a5fd1192864645411b65126ed60185c12e2d63c
HyunHub <19924___@naver.com>
2021-03-24 20:16:24 +0900

    Average - useCallback

--- hooks-tutorial/src/Average.js
+++ hooks-tutorial/src/Average.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from "react";
+import React, { useState, useMemo, useCallback } from "react";
 
 const getAverage = (numbers) => {
   console.log("평균값 계산 중,,");
@@ -11,15 +11,20 @@ const Average = () => {
   const [list, setList] = useState([]);
   const [number, setNumber] = useState("");
 
-  const onChange = (e) => {
+  const onChange = useCallback((e) => {
     setNumber(e.target.value);
-  };
+  }, []); // 컴포넌트가 처음 렌더링될 때만 함수 생성
+  // 기존 값을 조회하지 않고 바로 설정만 하기 때문에 배열이 비어있어도 상관 없음
 
-  const onInsert = (e) => {
-    const nextList = list.concat(parseInt(number));
-    setList(nextList);
-    setNumber("");
-  };
+  const onInsert = useCallback(
+    (e) => {
+      const nextList = list.concat(parseInt(number));
+      setList(nextList);
+      setNumber("");
+    },
+    [number, list]
+  ); // number 혹은 list가 바뀌었을 때만 함수 생성
+  // number와 list를 조회해서 nextList를 생성하기 때문에 배열 안에 꼭 넣어줘야 함
 
   const avg = useMemo(() => getAverage(list), [list]);
   return (
